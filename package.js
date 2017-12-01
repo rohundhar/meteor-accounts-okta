9a002502040bcde0f5f1ce9c3480279bb9757f8d
vurankar <vishramurankar@gmail.com>
2017-12-01 10:12:44 -0800

    update package name

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
-  name: 'vurankar:accounts-okta',
+  name: 'riffyn:accounts-okta',
   version: '0.1.0',
   summary: 'Meteor login service for Okta accounts.',
-  git: 'https://github.com/vurankar/meteor-accounts-okta',
+  git: 'https://github.com/RiffynInc/meteor-accounts-okta',
   documentation: 'README.md'
 });
 
@@ -14,7 +14,7 @@ Package.onUse(function(api) {
   // Export Accounts (etc) to packages using this one.
   api.imply('accounts-base', ['client', 'server']);
   api.use('accounts-oauth', ['client', 'server']);
-  api.use('vurankar:okta@0.1.0', ['client', 'server']);
+  api.use('riffyn:okta@0.1.0', ['client', 'server']);
 
   api.addFiles('okta.js');
 
